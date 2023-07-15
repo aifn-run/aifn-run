ca71bfafea35df84889465fe3967cea422d639ad
Darlan Alves <me@darlanalv.es>
2023-07-15 17:06:22 +0200

    chore: fix menu

--- App.vue
+++ App.vue
@@ -3,19 +3,26 @@
     class="w-screen h-screen flex flex-col overflow-hidden bg-gray-900 text-white"
   >
     <div
-      class="flex justify-items-center items-center border-b border-black bg-gray-700 space-x-12 shadow-lg p-4"
+      class="flex justify-between items-center border-b border-black bg-gray-700 space-x-12 shadow-lg p-4"
     >
       <h1 class="text-2xl font-bold text-white">ai.fn()</h1>
-      <nav class="mx-4 py-2 flex-1 flex" :data-length="links.length">
+      <nav class="py-2 flex-1 flex justify-center" :data-length="links.length">
         <router-link
-          v-for="route of links"
+          v-for="(route, index) of links"
           :key="route.path"
           :to="route.path"
           class="font-bold px-4 py-2 rounded shadow-lg border text-white"
+          :class="[
+            index === 0 && 'rounded-l',
+            index === links.length - 1 && 'rounded-r',
+          ]"
           active-class="bg-gray-200 text-gray-900"
           >{{ route.name }}</router-link
         >
       </nav>
+      <div>
+        <span class="material-icons text-4xl text-white">account_circle</span>
+      </div>
     </div>
 
     <div>
