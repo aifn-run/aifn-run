9720d0af38579cb6a7c799d95017eab1e0ee0f95
Darlan Alves <me@darlanalv.es>
2023-07-16 15:56:56 +0200

    chore: fix code blocks

--- components/guide/ApiDocs.vue
+++ components/guide/ApiDocs.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex space-x-8">
+  <div class="flex space-x-8" ref="page">
     <main class="w-3/4">
       <h1 class="text-2xl font-bold mb-6">Javascript API</h1>
 
-      <h2 id="create" class="text-lg mb-4">Create a function</h2>
-      <CodeBlock lang="javascript">
+      <h2 id="create" class="text-lg my-4">Create a function</h2>
+      <CodeBlock v-pre>
         // with just a prompt
         const f = await ai.fn('text for prompt');
 
@@ -12,8 +12,8 @@
         const g = await ai.fn({ p: 'text for prompt', name: 'lorem' });
       </CodeBlock>
 
-      <h2 id="use" class="text-lg mb-4">Use an AI function</h2>
-      <CodeBlock lang="javascript">const result = await f('input for AI to process');</CodeBlock>
+      <h2 id="use" class="text-lg my-4">Use an AI function</h2>
+      <CodeBlock>const result = await f('input for AI to process');</CodeBlock>
     </main>
     <aside class="w-1/4">
       <nav class="flex flex-col">
--- components/guide/GettingStarted.vue
+++ components/guide/GettingStarted.vue
@@ -1,29 +1,29 @@
 <template>
   <article>
     <section>
-      <h1>ai.fn() -- AI as a function</h1>
+      <h1 class="text-3xl font-bold pb-4">ai.fn() -- AI as a function</h1>
       <h2 class="text-2xl font-bold mb-4">Introduction</h2>
-      <p>This is ai.fn(), a service to use AI chat completions as a regular Javascript function in any project, for both browser and Node.JS.</p>
+      <p><strong>ai.fn()</strong> is a service to use AI chat completions as a regular Javascript function in any project, for both browser and Node.JS.</p>
 
-      <h2 class="text-2xl font-bold mb-4">But why?</h2>
-      <p>ChatBots and AI integrations via REST API's have their own boilerplate and set up, which has to be done over and over again.</p>
-      <p>Javascript already offers the tools we need to make that easier: async/await combined with ES module imports.</p>
-      <p>Not only that, with AI completions we can achieve many tasks using simple prompts, encapsulated as Javascript functions.</p>
+      <h2 class="text-2xl font-bold my-4">But why?</h2>
+      <p class="mb-4">ChatBots and AI integrations via REST API's have their own boilerplate and set up, which has to be done over and over again.</p>
+      <p class="mb-4">Javascript already offers the tools we need to make that easier: async/await combined with ES module imports.</p>
+      <p class="mb-4">Not only that, with AI completions we can achieve many tasks using simple prompts, encapsulated as Javascript functions.</p>
 
-      <h2 class="text-2xl font-bold mb-4">How to start (web)</h2>
-      <p>For web pages, import the library in your code:</p>
+      <h2 class="text-2xl font-bold my-4">How to start (web)</h2>
+      <p class="mb-4">For web pages, import the library in your code:</p>
       <CodeBlock>import ai from 'https://aifn.run/ai.mjs'</CodeBlock>
 
-      <p>Create an AI function:</p>
+      <p class="mb-4">Create an AI function:</p>
       <CodeBlock>const lorem = await ai.fn('Create a lorem ipsum paragraph with {count} words');</CodeBlock>
 
-      <p>And use the new function like any other:</p>
+      <p class="mb-4">And use the new function like any other:</p>
       <CodeBlock>const paragraph = await lorem({ count: 100 });</CodeBlock>
 
-      <h2 class="text-2xl font-bold mb-4">How to start (Node.js)</h2>
-      <p>For server-side, install `npm i @aifn/client`.</p>
-      <p>Then use the module in the same way the web API works:</p>
-      <CodeBlock>
+      <h2 class="text-2xl font-bold my-4">How to start (Node.js)</h2>
+      <p class="mb-4">For server-side, install `npm i @aifn/client`.</p>
+      <p class="mb-4">Then use the module in the same way the web API works:</p>
+      <CodeBlock v-pre>
         import ai from '@aifn/client';
 
         async function main() {
