c663e575d6daa9f26aac122d2e1ff56747fd175d
Darlan Alves <me@darlanalv.es>
2023-07-27 22:57:46 +0200

    fix: change text color in privacy text

--- components/about/Privacy.vue
+++ components/about/Privacy.vue
@@ -1,65 +1,70 @@
 <template>
-  <div class="prose lg:prose-l">
-    <article class="text-white">
-      <h1>Privacy Policy</h1>
+  <div class="prose-lg">
+    <h1>Privacy Policy</h1>
 
-      <p class="text-xs">Last updated: July 27, 2023</p>
+    <p class="text-xs">Last updated: July 27, 2023</p>
 
-      <p>
-        TL;DR:<br />
-        We use a cookie to save your session.<br />
-        We receive your name and email from Google on sign up.<br />
-        We use Google Analytics just to see if this project has any interest.<br />
-        That's it.
-      </p>
+    <p>
+      TL;DR:<br />
+      We use a cookie to save your session.<br />
+      We receive your name and email from Google on sign up.<br />
+      We use Google Analytics just to see if this project has any interest.<br />
+      That's it.
+    </p>
 
-      <p>
-        We respect your privacy and are committed to protecting it. This Privacy Policy describes our policies and
-        procedures on the collection, use, and disclosure of your information when you use our service and tells you
-        about your privacy rights and how the law protects you.
-      </p>
+    <p>
+      We respect your privacy and are committed to protecting it. This Privacy Policy describes our policies and
+      procedures on the collection, use, and disclosure of your information when you use our service and tells you about
+      your privacy rights and how the law protects you.
+    </p>
 
-      <h2>Information Collection and Use</h2>
+    <h2>Information Collection and Use</h2>
 
-      <p>
-        While using our service, we may ask you to provide us with certain personally identifiable information that can
-        be used to contact or identify you. Personally, identifiable information may include, but is not limited to your
-        name and email address.
-      </p>
+    <p>
+      While using our service, we may ask you to provide us with certain personally identifiable information that can be
+      used to contact or identify you. Personally, identifiable information may include, but is not limited to your name
+      and email address.
+    </p>
 
-      <h2>Use of Cookies</h2>
+    <h2>Use of Cookies</h2>
 
-      <p>
-        We use cookies and similar tracking technologies to track the activity on our service and hold certain
-        information.
-      </p>
+    <p>
+      We use cookies and similar tracking technologies to track the activity on our service and hold certain
+      information.
+    </p>
 
-      <p>
-        Cookies are files with a small amount of data which may include an anonymous unique identifier. Cookies are sent
-        to your browser from a website and stored on your device. Other tracking technologies are also used to collect
-        and track information and to improve and analyze our service.
-      </p>
+    <p>
+      Cookies are files with a small amount of data which may include an anonymous unique identifier. Cookies are sent
+      to your browser from a website and stored on your device. Other tracking technologies are also used to collect and
+      track information and to improve and analyze our service.
+    </p>
 
-      <p>
-        You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you
-        do not accept cookies, you may not be able to use some portions of this service.
-      </p>
+    <p>
+      You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do
+      not accept cookies, you may not be able to use some portions of this service.
+    </p>
 
-      <h2>Changes to this Privacy Policy</h2>
+    <h2>Changes to this Privacy Policy</h2>
 
-      <p>
-        We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy
-        Policy on this page.
-      </p>
+    <p>
+      We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy
+      Policy on this page.
+    </p>
 
-      <p>
-        You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are
-        effective when they are posted on this page.
-      </p>
+    <p>
+      You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are
+      effective when they are posted on this page.
+    </p>
 
-      <h2>Contact Us</h2>
+    <h2>Contact Us</h2>
 
-      <p>If you have any questions about this Privacy Policy, please contact me at homebots.inc@gmail.com.</p>
-    </article>
+    <p>If you have any questions about this Privacy Policy, please contact me at homebots.inc@gmail.com.</p>
   </div>
 </template>
+
+<style>
+.prose,
+.prose > * {
+  color: white !important;
+}
+</style>
