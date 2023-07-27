1215ec23e2f557815f539df1adcac0cd4fb62a38
Darlan Alves <me@darlanalv.es>
2023-07-27 22:52:03 +0200

    feat: add privacy policy

--- App.vue
+++ App.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="w-screen h-screen flex flex-col bg-gray-900 text-white">
-    <div
-      class="flex justify-between items-center border-b border-black bg-gray-700 space-x-2 shadow-lg p-4"
-    >
+    <div class="flex justify-between items-center border-b border-black bg-gray-700 space-x-2 shadow-lg p-4">
       <h1 class="text-xl font-bold text-white">
         <router-link to="/">ai.fn()</router-link>
       </h1>
@@ -12,10 +10,7 @@
           :key="route.path"
           :to="route.path"
           class="font-bold md:px-4 p-2 shadow-lg border text-white"
-          :class="[
-            index === 0 && 'rounded-l',
-            index === links.length - 1 && 'rounded-r',
-          ]"
+          :class="[index === 0 && 'rounded-l', index === links.length - 1 && 'rounded-r']"
           active-class="bg-gray-200 text-gray-900"
         >
           <span class="material-icons md:hidden text-lg">{{ route.icon }}</span>
@@ -37,25 +32,31 @@
 
     <div class="overflow-y-scroll mx-auto container py-8 px-4">
       <router-view></router-view>
+      <div class="bg-gray-700 text-center py-8 px-4 mt-8">
+        <router-link
+          v-for="route of footerPages"
+          :key="route.path"
+          :to="route.path"
+          class="px-4 inline-block"
+          active-class="bg-gray-200 text-gray-900"
+        >
+          <span class="text-sm">{{ route.name }}</span>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { useAuth } from "./composables/useAuth";
-import { useRouter } from "./composables/useRouter";
-import { computed } from "vue";
-import ai from "https://aifn.run/ai.mjs";
+import { useAuth } from './composables/useAuth';
+import { useRouter } from './composables/useRouter';
+import { computed } from 'vue';
 
-window.ai = ai;
-
-const { topPages, router } = useRouter();
+const { topPages, footerPages, router } = useRouter();
 const { isLoggedIn, profile, signIn } = useAuth();
-const links = computed(() =>
-  topPages.filter((page) => !page.protected || isLoggedIn.value)
-);
+const links = computed(() => topPages.filter((page) => !page.protected || isLoggedIn.value));
 
 function showProfile() {
-  router.push("/me");
+  router.push('/me');
 }
 </script>
--- composables/useRouter.ts
+++ composables/useRouter.ts
@@ -1,46 +1,57 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import GettingStarted from "../components/guide/GettingStarted.vue";
-import ApiDocs from "../components/guide/ApiDocs.vue";
-import Me from "../components/user/Me.vue";
-import Functions from "../components/functions/Functions.vue";
+import { createRouter, createWebHashHistory } from 'vue-router';
+import GettingStarted from '../components/guide/GettingStarted.vue';
+import ApiDocs from '../components/guide/ApiDocs.vue';
+import Me from '../components/user/Me.vue';
+import Functions from '../components/functions/Functions.vue';
+import Privacy from '../components/about/Privacy.vue';
 
 const topPages = [
   {
-    path: "/",
-    name: "Getting Started",
-    icon: "home",
+    path: '/',
+    name: 'Getting Started',
+    icon: 'home',
     component: GettingStarted,
   },
   {
-    path: "/api",
-    name: "API",
-    icon: "menu_book",
+    path: '/api',
+    name: 'API',
+    icon: 'menu_book',
     component: ApiDocs,
   },
   {
-    path: "/functions",
-    name: "Functions",
-    icon: "functions",
+    path: '/functions',
+    name: 'Functions',
+    icon: 'functions',
     protected: true,
     component: Functions,
   },
 ];
 
+const footerPages = [
+  {
+    path: '/ai/privacy',
+    name: 'Privacy Policy',
+    title: 'Privacy Policy',
+    component: Privacy,
+  },
+];
+
 const routes = [
   ...topPages,
+  ...footerPages,
   {
-    path: "/me",
-    name: "Me",
+    path: '/me',
+    name: 'Me',
     protected: true,
     component: Me,
   },
 ];
 
 export const router = createRouter({
-  history: createWebHashHistory("/"),
+  history: createWebHashHistory('/'),
   routes,
 });
 
 export function useRouter() {
-  return { router, routes, topPages };
+  return { router, routes, topPages, footerPages };
 }
