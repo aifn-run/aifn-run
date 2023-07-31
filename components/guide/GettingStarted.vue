0b1df3f9a72cc6231081ceea202630a7e19fa457
Darlan Alves <me@darlanalv.es>
2023-07-31 11:12:47 +0200

    fix: import library and pass in as argument

--- components/guide/GettingStarted.vue
+++ components/guide/GettingStarted.vue
@@ -4,16 +4,18 @@
       <h1 class="text-3xl font-bold pb-4">AI as a function</h1>
       <h2 class="text-2xl font-bold mt-8 mb-4">Introduction</h2>
       <p>
-        <strong>ai.fn() is a service to use AI chat completions as a regular Javascript function</strong> in any
-        project, for both browser and Node.JS.
+        <strong
+          >ai.fn() is a service to use AI chat completions as a regular
+          Javascript function</strong
+        >
+        in any project, for both browser and Node.JS.
       </p>
 
       <h2 class="text-2xl font-bold mb-4 mt-8">But why?</h2>
+      <p class="mb-4">ChatBots and AI services are all the rage these days!</p>
       <p class="mb-4">
-        ChatBots and AI services are all the rage these days!
-      </p>
-      <p class="mb-4">
-        And Javascript offers the tools we need to turn AI into code: "async/await' combined with "ES module imports".
+        And Javascript offers the tools we need to turn AI into code:
+        "async/await' combined with "ES module imports".
       </p>
       <p class="mb-4">
         Combining these two, we can complete tasks using AI and async functions.
@@ -22,7 +24,8 @@
       <h2 class="text-2xl font-bold mb-4 mt-8">How to start (web)</h2>
       <p class="mb-4">For web pages, import the library as a module:</p>
       <CodeBlock>
-        <pre v-pre>import ai from 'https://aifn.run/ai.mjs';
+        <pre v-pre>
+import ai from 'https://aifn.run/ai.mjs';
 
 async function main() {
   // Create an AI function
@@ -34,16 +37,20 @@ async function main() {
   console.log(paragraph);
 }
 
-main();</pre>
+main();</pre
+        >
       </CodeBlock>
 
       <h2 class="text-2xl font-bold mb-4 mt-8">How to start (Node.js)</h2>
       <p class="mb-4">
-        For server-side, install <code class="py-1 px-2 bg-gray-800">@aifn/client</code> and use the module in the same
-        way. This is more useful if you want to create an API or hide your prompts:
+        For server-side, install
+        <code class="py-1 px-2 bg-gray-800">@aifn/client</code> and use the
+        module in the same way. This is more useful if you want to create an API
+        or hide your prompts:
       </p>
       <CodeBlock>
-        <pre v-pre>import ai from '@aifn/client';
+        <pre v-pre>
+import ai from '@aifn/client';
 
 async function main() {
   const lorem = await ai.fn('Create a lorem ipsum paragraph with {count} words');
@@ -61,26 +68,39 @@ main();</pre
       <h2 class="text-2xl font-bold mb-4 mt-8">Try it</h2>
 
       <textarea
-        class="font-mono my-4 p-2 border border-gray-400 bg-gray-800 rounded-lg w-full h-half mb-4"
+        class="font-mono mb-4 p-2 border border-gray-400 bg-gray-800 rounded-lg w-full h-half"
         v-model="code"
       ></textarea>
 
       <div
-        v-if="output || running"
-        class="font-mono my-4 p-2 border border-gray-400 bg-gray-800 rounded-lg w-full mb-4"
+        v-if="output.length || running"
+        class="font-mono mb-4 p-2 border border-gray-400 bg-gray-800 rounded-lg w-full"
       >
-        <span class="material-icons animate-spin" v-if="running">autorenew</span>
-        {{ output }}
+        <div v-for="next in output" class="border-b border-gray-500 mb-2 py-2">
+          {{ next }}
+        </div>
+        <div v-if="running" class="border-b border-gray-500 mb-2 py-2">
+          <span class="material-icons animate-spin">autorenew</span>
+        </div>
+      </div>
+      <div class="text-right mb-4">
+        <button
+          type="button"
+          class="text-sm p-2 rounded"
+          @click="output.length = 0"
+        >
+          Clear
+        </button>
       </div>
 
-      <div>
+      <div class="mb-4">
         <button
           :disabled="running"
           @click="run()"
           class="text-white bg-blue-500 shadow-lg border border-blue-400 font-bold text-lg py-1 px-4 rounded flex mx-auto"
         >
           <span class="material-icons" :class="running && 'animate-spin'">{{
-            running ? 'refresh' : 'play_arrow'
+            running ? "refresh" : "play_arrow"
           }}</span>
           <span>Run</span>
         </button>
@@ -90,38 +110,36 @@ main();</pre
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import CodeBlock from './CodeBlock.vue';
+import ai from "https://aifn.run/ai.mjs";
+import { ref } from "vue";
+import CodeBlock from "./CodeBlock.vue";
 
 const running = ref(false);
 const initialSnippet = `
-// create an AI function
-const lorem = await ai.fn('Create a lorem ipsum paragraph with at most {count} words');
+const fillText = await ai.fn('Create a lorem ipsum paragraph with at most {count} words');
+const translate = await ai.fn('Translate the following text to English: {text}');
 
-// use the function to get an AI response
-const paragraph = await lorem({ count: 10 });
-
-console.log(paragraph);
+console.log(await fillText({ count: 10 }));
+console.log(await translate({ text: "Cada día sabemos más y entendemos menos" }));
 `;
 
 const code = ref(initialSnippet.trim());
-const output = ref('');
+const output = ref([]);
 
 async function run() {
-  output.value = '';
-  const fn = Function(`async function runExample() { ${code.value}; }
-  return runExample()`);
+  const fn = Function(`async function runExample(ai) { ${code.value}; }
+  return runExample(ai)`);
 
   running.value = true;
 
   try {
-    await fn();
+    await fn(ai);
   } finally {
     running.value = false;
   }
 }
 
-console.log = (text) => (output.value += text);
+console.log = (text) => output.value.push(text);
 </script>
 
 <style scoped>
