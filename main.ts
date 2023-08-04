faacc2440718c2dbe1b26176c234fe21b9244b74
Darlan Alves <me@darlanalv.es>
2023-08-04 10:46:52 +0200

    fix: highlight styles not working

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import { router } from './composables/useRouter.js';
+import "./highlight.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import { router } from "./composables/useRouter.js";
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).mount("#app");
