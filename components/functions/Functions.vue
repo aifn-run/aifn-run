210336d934be3c053e9511fc7adb99f74975f881
Darlan Alves <me@darlanalv.es>
2023-09-01 17:14:18 +0200

    chore: fix layout

--- components/functions/Functions.vue
+++ components/functions/Functions.vue
@@ -2,9 +2,8 @@
   <div>
     <h1 class="text-2xl font-bold mb-6 flex items-center justify-between">
       Your Functions
-      <button class="p-4 flex items-center" @click.prevent="addFunction()">
-        <span class="material-icons">add</span>
-        New function
+      <button class="py-2 px-4 bg-gray-200 text-gray-900 rounded text-base" @click.prevent="addFunction()">
+        new function
       </button>
     </h1>
     <div class="my-8 border border-gray-200 rounded-lg overflow-hidden">
@@ -20,17 +19,10 @@
 
       <div v-for="fn of functions" class="px-4 border-b border-gray-200">
         <div class="flex items-center justify-between">
-          <button @click="fn.editing || editItem(fn)">
+          <button @click.prevent="onToggle(fn)">
             <span class="font-mono">{{
               (fn.tmp && fn.tmp.name) || fn.item.name || fn.item.uid
             }}</span>
-          </button>
-          <button
-            @click.prevent="
-              (fn.editing && (fn.editing = false)) || editItem(fn)
-            "
-            class="p-2"
-          >
             <span class="material-icons"
               >{{ (fn.editing && "close") || "expand_more" }}
             </span>
@@ -58,24 +50,28 @@ import Editor from "./Editor.vue";
 const { listFunctions } = useFunctions();
 
 const functions = ref<
-  Array<{ item: FunctionBody; tmp: FunctionBody; editing: boolean }>
+  Array<{ fn: FunctionBody; tmp: FunctionBody; editing: boolean }>
 >([]);
 
-async function editItem(item) {
-  item.tmp = { ...item.fn };
-  item.editing = true;
+function onToggle(fn) {
+  if (fn.editing) {
+    return (fn.editing = false);
+  }
+
+  fn.tmp = { ...fn.item };
+  fn.editing = true;
 }
 
 function addFunction() {
   const fn = { p: "", name: "-" };
-  functions.value.push({ item: fn, tmp: fn, editing: true });
+  functions.value.unshift({ fn, tmp: fn, editing: true });
 }
 
 async function loadFunctions() {
   const list = await listFunctions();
-  functions.value = list.map((fn) => ({
-    item: fn,
-    tmp: { ...fn },
+  functions.value = list.map((fn: any) => ({
+    fn,
+    tmp: {},
     editing: false,
   }));
 }
